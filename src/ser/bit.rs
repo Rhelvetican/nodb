747c0534ad95ee60e99b12f7789c4593033a32ee
Rh4096 <rhelvetican@gmail.com>
2024-06-05 19:18:58 +0700

    feat: add bitcode serialization method

---
+++ src/ser/bit.rs
@@ -0,0 +1,31 @@
+use super::SerializeMethod;
+use crate::{DbListMap, DbMap};
+use anyhow::{anyhow, Result};
+use bitcode::{deserialize, serialize};
+use serde::{de::DeserializeOwned, Serialize};
+
+pub struct BitSer {}
+
+impl BitSer {
+    pub fn new() -> Self {
+        BitSer {}
+    }
+}
+
+impl SerializeMethod for BitSer {
+    fn serialize_data<T: Serialize>(&self, data: &T) -> Result<Vec<u8>> {
+        Ok(serialize(data)?)
+    }
+    fn serialize_db(&self, db_map: &DbMap, db_list_map: &DbListMap) -> Result<Vec<u8>> {
+        self.serialize_data(&(db_map, db_list_map))
+    }
+    fn deserialize_data<T: DeserializeOwned>(&self, data: &[u8]) -> Option<T> {
+        deserialize(data).ok()
+    }
+    fn deserialized_db(&self, ser_db: &[u8]) -> Result<(DbMap, DbListMap)> {
+        match self.deserialize_data(ser_db) {
+            Some((db_map, db_list_map)) => Ok((db_map, db_list_map)),
+            None => Err(anyhow!("Failed to deserialize db")),
+        }
+    }
+}
--- src/ser/mod.rs
+++ src/ser/mod.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use bin::BinSer;
+use bit::BitSer;
 use cbor::CborSer;
 use json::JsonSer;
 use serde::{de::DeserializeOwned, Serialize};
@@ -9,6 +10,7 @@ use toml::TomlSer;
 use crate::{DbListMap, DbMap};
 
 mod bin;
+mod bit;
 mod cbor;
 mod json;
 mod toml;
@@ -20,6 +22,7 @@ pub enum SerializationMethod {
     Bin,
     Cbor,
     Toml,
+    Bit,
 }
 
 impl<T: Into<usize>> From<T> for SerializationMethod {
@@ -30,6 +33,7 @@ impl<T: Into<usize>> From<T> for SerializationMethod {
             1 => SerializationMethod::Bin,
             2 => SerializationMethod::Cbor,
             3 => SerializationMethod::Toml,
+            4 => SerializationMethod::Bit,
             _ => SerializationMethod::Json,
         }
     }
@@ -53,6 +57,7 @@ pub(crate) enum Serializer {
     Bin(BinSer),
     Cbor(CborSer),
     Toml(TomlSer),
+    Bit(BitSer),
 }
 
 impl From<SerializationMethod> for Serializer {
@@ -62,6 +67,7 @@ impl From<SerializationMethod> for Serializer {
             SerializationMethod::Bin => Serializer::Bin(BinSer::new()),
             SerializationMethod::Cbor => Serializer::Cbor(CborSer::new()),
             SerializationMethod::Toml => Serializer::Toml(TomlSer::new()),
+            SerializationMethod::Bit => Serializer::Bit(BitSer::new()),
         }
     }
 }
@@ -73,6 +79,7 @@ impl SerializeMethod for Serializer {
             Serializer::Bin(bin_ser) => bin_ser.serialize_data(data),
             Serializer::Cbor(cbor_ser) => cbor_ser.serialize_data(data),
             Serializer::Toml(toml_ser) => toml_ser.serialize_data(data),
+            Serializer::Bit(bit_ser) => bit_ser.serialize_data(data),
         }
     }
     fn serialize_db(&self, db_map: &DbMap, db_list_map: &DbListMap) -> Result<Vec<u8>> {
@@ -81,6 +88,7 @@ impl SerializeMethod for Serializer {
             Serializer::Bin(bin_ser) => bin_ser.serialize_db(db_map, db_list_map),
             Serializer::Cbor(cbor_ser) => cbor_ser.serialize_db(db_map, db_list_map),
             Serializer::Toml(toml_ser) => toml_ser.serialize_db(db_map, db_list_map),
+            Serializer::Bit(bit_ser) => bit_ser.serialize_db(db_map, db_list_map),
         }
     }
     fn deserialize_data<T: DeserializeOwned>(&self, data: &[u8]) -> Option<T> {
@@ -89,6 +97,7 @@ impl SerializeMethod for Serializer {
             Serializer::Bin(bin_ser) => bin_ser.deserialize_data(data),
             Serializer::Cbor(cbor_ser) => cbor_ser.deserialize_data(data),
             Serializer::Toml(toml_ser) => toml_ser.deserialize_data(data),
+            Serializer::Bit(bit_ser) => bit_ser.deserialize_data(data),
         }
     }
     fn deserialized_db(&self, ser_db: &[u8]) -> Result<(DbMap, DbListMap)> {
@@ -97,6 +106,7 @@ impl SerializeMethod for Serializer {
             Serializer::Bin(bin_ser) => bin_ser.deserialized_db(ser_db),
             Serializer::Cbor(cbor_ser) => cbor_ser.deserialized_db(ser_db),
             Serializer::Toml(toml_ser) => toml_ser.deserialized_db(ser_db),
+            Serializer::Bit(bit_ser) => bit_ser.deserialized_db(ser_db),
         }
     }
 }
