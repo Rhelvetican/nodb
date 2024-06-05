79430a9ff56d365a93355d6005fe7282ef70f1f6
Rh4096 <rhelvetican@gmail.com>
2024-06-05 18:52:59 +0700

    feat: toml format support

--- src/ser/mod.rs
+++ src/ser/mod.rs
@@ -4,12 +4,14 @@ use cbor::CborSer;
 use json::JsonSer;
 use serde::{de::DeserializeOwned, Serialize};
 use std::fmt::{Display, Formatter, Result as FmtResult};
+use toml::TomlSer;
 
 use crate::{DbListMap, DbMap};
 
 mod bin;
 mod cbor;
 mod json;
+mod toml;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
 pub enum SerializationMethod {
@@ -17,6 +19,7 @@ pub enum SerializationMethod {
     Json,
     Bin,
     Cbor,
+    Toml,
 }
 
 impl<T: Into<usize>> From<T> for SerializationMethod {
@@ -26,6 +29,7 @@ impl<T: Into<usize>> From<T> for SerializationMethod {
             0 => SerializationMethod::Json,
             1 => SerializationMethod::Bin,
             2 => SerializationMethod::Cbor,
+            3 => SerializationMethod::Toml,
             _ => SerializationMethod::Json,
         }
     }
@@ -48,6 +52,7 @@ pub(crate) enum Serializer {
     Json(JsonSer),
     Bin(BinSer),
     Cbor(CborSer),
+    Toml(TomlSer),
 }
 
 impl From<SerializationMethod> for Serializer {
@@ -56,6 +61,7 @@ impl From<SerializationMethod> for Serializer {
             SerializationMethod::Json => Serializer::Json(JsonSer::new()),
             SerializationMethod::Bin => Serializer::Bin(BinSer::new()),
             SerializationMethod::Cbor => Serializer::Cbor(CborSer::new()),
+            SerializationMethod::Toml => Serializer::Toml(TomlSer::new()),
         }
     }
 }
@@ -66,6 +72,7 @@ impl SerializeMethod for Serializer {
             Serializer::Json(json_ser) => json_ser.serialize_data(data),
             Serializer::Bin(bin_ser) => bin_ser.serialize_data(data),
             Serializer::Cbor(cbor_ser) => cbor_ser.serialize_data(data),
+            Serializer::Toml(toml_ser) => toml_ser.serialize_data(data),
         }
     }
     fn serialize_db(&self, db_map: &DbMap, db_list_map: &DbListMap) -> Result<Vec<u8>> {
@@ -73,6 +80,7 @@ impl SerializeMethod for Serializer {
             Serializer::Json(json_ser) => json_ser.serialize_db(db_map, db_list_map),
             Serializer::Bin(bin_ser) => bin_ser.serialize_db(db_map, db_list_map),
             Serializer::Cbor(cbor_ser) => cbor_ser.serialize_db(db_map, db_list_map),
+            Serializer::Toml(toml_ser) => toml_ser.serialize_db(db_map, db_list_map),
         }
     }
     fn deserialize_data<T: DeserializeOwned>(&self, data: &[u8]) -> Option<T> {
@@ -80,6 +88,7 @@ impl SerializeMethod for Serializer {
             Serializer::Json(json_ser) => json_ser.deserialize_data(data),
             Serializer::Bin(bin_ser) => bin_ser.deserialize_data(data),
             Serializer::Cbor(cbor_ser) => cbor_ser.deserialize_data(data),
+            Serializer::Toml(toml_ser) => toml_ser.deserialize_data(data),
         }
     }
     fn deserialized_db(&self, ser_db: &[u8]) -> Result<(DbMap, DbListMap)> {
@@ -87,6 +96,7 @@ impl SerializeMethod for Serializer {
             Serializer::Json(json_ser) => json_ser.deserialized_db(ser_db),
             Serializer::Bin(bin_ser) => bin_ser.deserialized_db(ser_db),
             Serializer::Cbor(cbor_ser) => cbor_ser.deserialized_db(ser_db),
+            Serializer::Toml(toml_ser) => toml_ser.deserialized_db(ser_db),
         }
     }
 }
