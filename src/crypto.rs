02e8bd29984167c04a394c862c4b3976a5e4ec87
Rh4096 <rhelvetican@gmail.com>
2024-06-07 00:38:24 +0700

    rudimentary fw

--- src/crypto.rs
+++ src/crypto.rs
@@ -10,15 +10,24 @@ use base64::{
 
 const STD: GeneralPurpose = STANDARD;
 
+/// The `B64` struct is used to encrypt and decrypt data using the `base64` algorithm.
 pub struct B64 {}
 
 impl B64 {
+    /// Creates a new `B64` instance.
+
     pub const fn new() -> Self {
         Self {}
     }
+
+    /// Encrypts the given data using the `base64` algorithm.
+
     pub fn encrypt<T: AsRef<[u8]>>(&self, data: T) -> String {
         STD.encode(data)
     }
+
+    /// Decrypts the given data using the `base64` algorithm.
+
     pub fn decrypt<T: AsRef<[u8]>>(&self, data: T) -> Result<Vec<u8>> {
         Ok(STD.decode(data)?)
     }
--- src/ext.rs
+++ src/ext.rs
@@ -2,15 +2,38 @@ use serde::Serialize;
 
 use crate::nodb::NoDb;
 
+/// A struct for extending NoDb lists and adding more items to them.
 pub struct NoDbExt<'a> {
     pub(crate) db: &'a mut NoDb,
     pub(crate) list_name: String,
 }
 
 impl<'a> NoDbExt<'a> {
+    /// Add a single item to an existing list.
+    ///
+    /// As mentioned before, the lists are heterogeneous, meaning a single list can contain
+    /// items of different types. That means that the item can be of any type that is serializable.
+    /// That includes all primitive types, vectors, tuples and every struct that has the
+    /// `#[derive(Serialize, Deserialize)` attribute.
+    /// The method returns another `NoDbExt` object that enables to continue adding
+    /// items to the list.
+
     pub fn ladd<V: Serialize>(&mut self, value: V) -> Option<NoDbExt> {
         self.db.ladd(&self.list_name, &value)
     }
+
+    /// Add multiple items to an existing list.
+    ///
+    /// As mentioned before, the lists are heterogeneous, meaning a single list can contain
+    /// items of different types. That means that the item can be of any type that is serializable.
+    /// That includes all primitive types, vectors, tuples and every struct that has the
+    /// `#[derive(Serialize, Deserialize)` attribute.
+    /// This method adds multiple items to the list, but since they're in a vector that means all
+    /// of them are of the same type. Of course it doesn't mean that the list cannot contain items
+    /// of other types as well, as you can see in the example below.
+    /// The method returns another `NoDbExt` object that enables to continue adding
+    /// items to the list.
+
     pub fn lextend<'b, V, I>(&mut self, seq: I) -> Option<NoDbExt>
     where
         V: 'b + Serialize,
