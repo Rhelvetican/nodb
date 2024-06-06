16d867cf77ac7a264e6b826f3ca42333b9817ced
Rh4096 <rhelvetican@gmail.com>
2024-06-07 00:33:40 +0700

    doc 2

--- src/iter.rs
+++ src/iter.rs
@@ -4,6 +4,7 @@ use serde::de::DeserializeOwned;
 
 use crate::ser::{SerializeMethod, Serializer};
 
+/// Iterator object for iterating over keys and values in NoDb. Returned in [NoDb::iter()](struct.NoDb.html#method.iter)
 pub struct NoDbIter<'a> {
     pub(crate) map_iter: HashMapIter<'a, String, Vec<u8>>,
     pub(crate) ser: &'a Serializer,
@@ -23,6 +24,7 @@ impl<'a> Iterator for NoDbIter<'a> {
     }
 }
 
+/// The object returned in each iteration when iterating over keys and values in NoDb
 pub struct NoDbIterItem<'a> {
     key: &'a str,
     val: &'a Vec<u8>,
@@ -30,9 +32,22 @@ pub struct NoDbIterItem<'a> {
 }
 
 impl<'a> NoDbIterItem<'a> {
+    /// Get the key
+
     pub fn get_key(&self) -> &str {
         self.key
     }
+
+    /// Get the value of the key.
+    ///
+    /// The key is always a string but the value can be of any type. It's the user's
+    /// responsibility to know the value type and give it while calling this method.
+    /// If the key doesn't exist or if the type is wrong, `None` will be returned.
+    /// Otherwise `Some(V)` will be returned.
+    /// Since the values are stored in a serialized way the returned object is
+    /// not a reference to the value stored in a DB but actually a new instance of it.
+    /// The method returns `Some(V)` if deserialization succeeds or `None` otherwise.
+
     pub fn get_value<V>(&self) -> Option<V>
     where
         V: DeserializeOwned,
@@ -41,6 +56,7 @@ impl<'a> NoDbIterItem<'a> {
     }
 }
 
+/// Iterator object for iterating over items in a NoDb list. Returned in [NoDb::liter()](struct.NoDb.html#method.liter)
 pub struct NoDbListIter<'a> {
     pub(crate) list_iter: SliceIter<'a, Vec<u8>>,
     pub(crate) ser: &'a Serializer,
@@ -59,12 +75,21 @@ impl<'a> Iterator for NoDbListIter<'a> {
     }
 }
 
+/// The object returned in each iteration when iterating over a NoDb list
 pub struct NoDbListIterItem<'a> {
     val: &'a Vec<u8>,
     ser: &'a Serializer,
 }
 
 impl<'a> NoDbListIterItem<'a> {
+    /// Get the item in the current position.
+    ///
+    /// This method retrieves the item in the current position. It's the user's responsibility
+    /// to know what is the correct type of the item and give it while calling this method.
+    /// Since the item in the lists are stored in a serialized way the returned object
+    /// is not a reference to the item stored in a DB but actually a new instance of it.
+    /// The method returns `Some(V)` if deserialization succeeds or `None` otherwise.
+    ///
     pub fn get_item<V: DeserializeOwned>(&self) -> Option<V> {
         self.ser.deserialize_data(self.val)
     }
